a8943dadbf0f2557e0c19518e8da123922756383
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-06-09 23:38:53 +0300

    feat: calculations

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-﻿
+﻿let num = 266219;
+let stringNum = num.toString();
+let mulResult = 1;
+
+for (let i = 0; i < stringNum.length; i++) {
+  mulResult *= Number(stringNum[i]);
+}
+
+mulResult **= 3;
+console.log(mulResult.toString().slice(-2));
