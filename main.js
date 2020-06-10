e7fca748a7907a938d2125d5f887cf227407acab
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-06-10 23:23:39 +0300

    feat: functions

--- main.js
+++ main.js
@@ -1,39 +1,23 @@
 ﻿'use strict';
 
-// first
-let lang = prompt("en / ru");
+const cropString = function(argument) {
+  if (typeof argument !== 'string') {
+    return 'Передана не строка';
+  } else {
+    let result = argument.trim();
 
-if (lang === "ru") {
-  console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
-}
-else if (lang === "en") {
-  console.log("monday, tuesday, wednesday, thursday, friday, saturday, sunday");
-}
-
-switch (lang) {
-  case 'ru':
-    console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
-    break;
-  case 'en':
-    console.log("monday, tuesday, wednesday, thursday, friday, saturday, sunday");
-    break;
+    if (result.length > 30) {
+      return result.substring(0, 30) + '...';
+    } else {
+      return result;
+    }
+  }
 }
 
-let daysOfWeek = [];
-daysOfWeek["ru".charCodeAt(0) + "ru".charCodeAt(1)] = "понедельник, вторник, среда, четверг, пятница, суббота, воскресенье";
-daysOfWeek["en".charCodeAt(0) + "en".charCodeAt(1)] = "monday, tuesday, wednesday, thursday, friday, saturday, sunday";
+console.log(cropString(prompt('Введите аргумент')));
 
-console.log(daysOfWeek[lang.charCodeAt(0) + lang.charCodeAt(1)]);
-
-// bonus to first
-console.log('a bonus:');
-
-let daysOfWeekObj = { 'ru' : "понедельник, вторник, среда, четверг, пятница, суббота, воскресенье", 'en' : "monday, tuesday, wednesday, thursday, friday, saturday, sunday"};
-
-console.log(daysOfWeekObj[lang]);
-
-// second
-
-let namePerson = prompt('Введите имя');
-
-console.log(namePerson === 'Артем' ? "директор" : (namePerson === 'Максим' ? 'преподаватель' : 'студент'));
+console.log('Собственная проверка:');
+console.log(cropString(1));
+console.log(cropString("Проверка"));
+console.log(cropString("     Проверка         "));
+console.log(cropString("Корабли лавировали, лавировали, да не вылавировали"));
